82f97dc5342a07eb0b807b49aba0c3caee6313be
idaira <idairaalemanquintana@gmail.com>
2023-10-08 16:56:55 +0100

    added spinner test

--- modulo-05-frameworks/00-boilerplate/src/common/components/spinner/spinner.component.tsx
+++ modulo-05-frameworks/00-boilerplate/src/common/components/spinner/spinner.component.tsx
@@ -7,10 +7,13 @@ import * as classes from './spinner.styles';
 export const SpinnerComponent: React.FunctionComponent = () => {
   const { promiseInProgress } = usePromiseTracker();
   return (
+
+    <>
     <Modal open={promiseInProgress} className={classes.modal}>
-      <div className={classes.loaderContainer}>
+      <div data-testid='spinner' className={classes.loaderContainer}>
         <Loader />
       </div>
     </Modal>
+    </>
   );
 };
